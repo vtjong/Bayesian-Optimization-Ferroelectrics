bff04d0f1d47f7d9f078bbda59297986a6861b92
Valene Tjong <valenetjong@Valenes-MBP.attlocal.net>
2023-01-25 03:34:06 -0800

    code clean-up

--- src/preprocess_tested.py
+++ src/preprocess_tested.py
@@ -248,7 +248,7 @@ def read_file(dir, idx):
 
 def file_combine(dir, idx):
     """
-    file_combine(dir, idx) combines files with same device and different cycles
+    file_combine(files_exp) combines files with same device and different cycles
     into single file with multiple sheets:
         i.e. KHM0ID_subID_PUND_devspecs_3cycles_1e6_1e7cycles.xls
     """
@@ -256,54 +256,60 @@ def file_combine(dir, idx):
     files = subdir + '/*.xls'
     files_exp = glob.glob(files, recursive = True)
 
-    # Set-up for file iteration
     end_files = [file for file in files_exp if "endurance" in file and "PV" not in file]
     PV_files = [file for file in files_exp if "PV" in file]
     PUND_files = [file for file in files_exp if "PUND" in file]
     files_col = [PV_files, PUND_files, end_files]
+    
     devs = set([get_dev("PV_", PV_file)for PV_file in PV_files])
     make_dict = lambda: dict.fromkeys(devs, [])
     PV_dict, PUND_dict, end_dict = make_dict(), make_dict(), make_dict()
     file_dicts = [PV_dict, PUND_dict, end_dict]
     
     for f_d, fs in list(zip(file_dicts, files_col)): file_dict_maker(f_d, fs, devs)
-
-    # File interation and combine
-    app = xw.App(visible=False)
-    for type, f_d in [('PV', PV_dict), ('end', end_dict), ('PUND', PUND_dict)]: 
-        file_combiner(type, f_d)
-    app.quit()
-
-def file_combiner(type, f_d):
-    """
-    file_combiner(type, f_d) performs actual combining of files with same device 
-    as helper for file_combine(dir, idx). 
-    """
     get_iter = lambda fn:fn[fn.rfind('_'):]
     cut_ext = lambda fn, cut_word:fn[:fn.rfind(cut_word)]
-    for dev, fn_list in f_d.items():
-        fn_to = fn_list[-1]
-        iter = range(len(fn_list)-1)
-        if type != "PUND":
+
+    app = xw.App(visible=False)
+    for f_d in [PV_dict, end_dict]: 
+        for dev, fn_list in f_d.items():
             fn_to = fn_list[0]
             fn_temp = cut_ext(fn_to, '.') + "_after_3cycles.xls"
             os.rename(fn_to, fn_temp)
             fn_to = fn_temp
-            iter = range(1, len(fn_list))
-        for fn_idx in iter:
             wb_to = xw.Book(fn_to)
-            fn_from = fn_list[fn_idx]
-            wb_from = xw.Book(fn_from)
-            ws_from = wb_from.sheets['Data']
-            ws_from.copy(after=wb_to.sheets[-1])
-            wb_to.sheets[-1].name = 'Append' + str(2+fn_idx) if type != "PUND" else 'Append' + str(1+fn_idx)
-            wb_from.close()
-            os.remove(fn_from)
-            wb_to.save()
-            wb_to.close()
-            fn_temp = cut_ext(fn_to, 'cycles')+ get_iter(fn_from)
-            os.rename(fn_to, fn_temp)
-            fn_to = fn_temp
+            for fn_idx in range(1, len(fn_list)):
+                fn_from = fn_list[fn_idx]
+                wb_from = xw.Book(fn_from)
+                ws_from = wb_from.sheets['Data']
+                ws_from.copy(after=wb_to.sheets[-1])
+                wb_to.sheets[-1].name = 'Append' + str(2+fn_idx)
+                wb_from.close()
+                os.remove(fn_from)
+                wb_to.save()
+                wb_to.close()
+                fn_temp = cut_ext(fn_to, 'cycles')+ get_iter(fn_from)
+                os.rename(fn_to, fn_temp)
+                fn_to = fn_temp
+        
+    for f_d in [PUND_dict]:
+        for dev, fn_list in f_d.items():
+            fn_to = fn_list[-1]
+            for fn_idx in range(len(fn_list)-1):
+                wb_to = xw.Book(fn_to)
+                fn_from = fn_list[fn_idx]
+                wb_from = xw.Book(fn_from)
+                ws_from = wb_from.sheets['Data']
+                ws_from.copy(after=wb_to.sheets[-1])
+                wb_to.sheets[-1].name = 'Append' + str(1+fn_idx)
+                wb_from.close()
+                os.remove(fn_from)
+                wb_to.save()
+                wb_to.close()
+                fn_temp = cut_ext(fn_to, 'cycles')+ get_iter(fn_from)
+                os.rename(fn_to, fn_temp)
+                fn_to = fn_temp
+    app.quit()
 
 def file_dict_maker(file_dict, files, devs):
     for dev in devs:
