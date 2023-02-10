4257e952cc419c94dfd70d570479183d3b51d26a
vtjong <vyt3@cornell.edu>
2023-02-10 04:53:54 -0500

    Changes and runs

---
+++ test.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# plt.plot([100,1024,10240],[80,7.8,5.78], 'o-', c = '#580F41')
+# plt.plot(5, c = '#A9561E')
+# plt.ylabel('delay time (ms)')
+# plt.xlabel('bandwidth (Kbs)')
+# plt.title(device + " PUND "+ iter)
+# plt.legend(loc="upper right")
+# plt.savefig(dir + "/PUND/" + device + "_" + iter +"_PUND-plot")  
+# plt.show() 
+
+print(np.ones([2]))
+print([1,1])
+n=30
+print([n for i in range(2)])
