1a955059d031575505d2a72755106bc0898ecd57
vtjong <vyt3@cornell.edu>
2023-02-10 04:53:54 -0500

    Plotting updates

--- src/datasetmaker.py
+++ src/datasetmaker.py
@@ -8,44 +8,6 @@ import plotly.express as px
 sys.path.append('..')
 sys.path.insert(0, '../src')
 
-def make_dummy_data(dir = "/Users/valenetjong/Bayesian-Optimization-Ferroelectrics/data/",
-    src_file="KHM005_KHM006_quartz_HZO_samples.csv", 
-    output_file_name='../data/KHM005_KHM006_quartz_HZO_samples2.csv'): 
-    """
-    make_dummy_data() reads in [src_file], containing Thickness (nm) and 
-    Flash Time (msec) data, as well as fills in dummy Duty Cycle and 
-    Num Pulses data to a pandas dataframe, which is returned. 
-    A csv of this training data, with the four aforementioned input parameters, 
-    is outputted to [output_file_name].
-    """
-    # Load data
-    fe_data = pd.read_csv(dir + src_file, index_col=0)
-    fe_data_len = len(fe_data['Thickness (nm)'])
-
-    # Add duty cycle data
-    duty_cycle_list = np.array([0.45, 0.55, 0.65])
-    duty_cycles = np.random.choice(duty_cycle_list, size=fe_data_len)
-    fe_data['Duty Cycle'] = duty_cycles
-
-    # Add num pulses data
-    num_pulses_list = np.array([15, 25])
-    num_pulses = np.random.choice(num_pulses_list, size=fe_data_len)
-    fe_data['Num Pulses'] = num_pulses
-
-    # Rearrange columns
-    cols = list(fe_data.columns.values) 
-    idx = cols.index("Flash time (msec)")
-    cols = cols[:idx+1] + cols[-2:] + cols[idx+1:-2]
-    fe_data = fe_data[cols]
-
-    # Write data back to csv
-    os.makedirs('../data', exist_ok=True) 
-    fe_data.to_csv('../data/KHM005_KHM006_quartz_HZO_samples2.csv') 
-
-    # Load manipulated data
-    fe_data = pd.read_csv(output_file_name, index_col=0)
-    return fe_data
-
 def read_dat(dir="/Users/valenetjong/Bayesian-Optimization-Ferroelectrics/data/",
             src_file = "Data KHM010XX.xlsx", sheet= "3 cycles", out_file = "KHM010XX.csv"):
     file = dir + src_file
@@ -117,7 +79,7 @@ def dataset():
     dataset() serves as main, to call the other utility functions.
     """
     fe_data = read_dat()
-    display_data(fe_data)
+    # display_data(fe_data)
     train_x, train_y = datasetmaker(fe_data)
     num_params, test_grid, test_x = grid_maker(train_x)
     return train_x, train_y, num_params, test_grid, test_x
--- src/plotter.py
+++ src/plotter.py
@@ -6,6 +6,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter1d
 from adjustText import adjust_text
+import plotly.graph_objects as go
 
 def prettyplot():
     """
@@ -79,3 +80,76 @@ def vis_iv(iv_data, iv_filt, pos_tup, neg_tup, device):
     plt.legend(loc="upper right")
     plt.savefig(dir + "/IV/" + device + "_" + "IV-plot")    
     # plt.show()
+
+def vis_pred(train_x, train_y, test_grid, obs, nshape):
+    pred_labels = obs.mean.view(nshape)
+    fig = go.Figure(data=[go.Surface(z=pred_labels.numpy().T, 
+                                    x=test_grid[:,0], 
+                                    y=test_grid[:,1], 
+                                    name='GP regression')])
+    fig.add_trace(go.Scatter3d(x=train_x[:,0].numpy(), y=train_x[:,1].numpy(),  
+                            z=train_y.numpy(), mode='markers', 
+                            marker={'color':'darkgreen'}, name='training data'))
+    fig.update_layout( width=1000, height=800,
+                    legend=dict(orientation="h", yanchor="top", y=1.02, 
+                    xanchor="left",x=1), margin=dict(r=20, l=10, b=10, t=10), 
+                        scene=dict(
+                        xaxis_title="Voltage (V)",
+                        yaxis_title="Pulse Width (msec)",
+                        zaxis_title='2 Qsw/(U+|D|)')
+                    )
+    camera = dict(
+        up=dict(x=0, y=0, z=1),
+        center=dict(x=0, y=0, z=0),
+        eye=dict(x=-2, y=-2.5, z=1.75)
+    )
+
+    fig.update_layout(scene_camera=camera)
+    fig.show()
+
+def vis_acq(train_x, train_y, test_grid, pred_labels, upper_surf, lower_surf, ucb, th, pi, ei, ca):
+    fig = go.Figure(data=[go.Surface(z = upper_surf, x=test_grid[:,0], 
+                    y=test_grid[:,1], opacity=0.5, showscale=False)])
+    fig.add_trace(go.Surface(z = lower_surf, x=test_grid[:,0], y=test_grid[:,1], 
+                            opacity=0.2, showscale=False))
+    fig.add_trace(go.Scatter3d(x=train_x[:,0].numpy(), 
+                                y=train_x[:,1].numpy(), 
+                                z=train_y.numpy(), 
+                                mode='markers', 
+                                name='training data', 
+                                marker={'color':'darkgreen'}))
+
+    fig.add_trace(go.Scatter3d(x=test_grid[ucb[1], 0].numpy(), 
+                                y=test_grid[ucb[0], 1].numpy(), 
+                                z=[pred_labels[ucb[0], ucb[1]]], mode='markers', 
+                                name='max(upper confidence bound)')) 
+
+    fig.add_trace(go.Scatter3d(x=test_grid[th[1], 0].numpy(), 
+                                y=test_grid[th[0],1].numpy(), 
+                                z=[pred_labels[th[0], th[1]].detach().numpy()],
+                                 mode='markers', name='max(thompson)')) 
+    fig.add_trace(go.Scatter3d(x=test_grid[pi[1], 0].numpy(),
+                                y=test_grid[pi[0], 1].numpy(), 
+                                z=[pred_labels[pi[0], pi[1]]], 
+                                mode='markers', name='max(pi)'))
+
+    fig.add_trace(go.Scatter3d(x=test_grid[ei[1], 0].numpy(), 
+                            y=test_grid[ei[0], 1].numpy(), 
+                            z=[pred_labels[ei[0], ei[1]]], mode='markers', 
+                            name='max(ei)'))
+
+    fig.add_trace(go.Scatter3d(x=test_grid[ca[1], 0].numpy(), 
+                                y=test_grid[ca[0], 1].numpy(), 
+                                z=[pred_labels[ca[0], ca[1]]], 
+                                mode='markers', name='max(ca)'))
+
+    fig.update_layout( width=800, height=600,
+                    margin=dict(r=20, l=10, b=10, t=10),
+                    legend=dict(orientation="h", yanchor="bottom", 
+                                y=1.02, xanchor="right",x=1),
+                    scene=dict(
+                        xaxis_title="Voltage (V)",
+                        yaxis_title="Pulse Width (msec)",
+                        zaxis_title='2 Qsw/(U+|D|)')
+                    )
+    fig.show()
