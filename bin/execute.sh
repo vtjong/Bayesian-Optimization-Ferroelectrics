297d57bb5164fb8b190a4d8e9346a1d6fba704ca
vtjong <vyt3@cornell.edu>
2023-08-04 07:24:27 -0400

    add bash script run

---
+++ bin/execute.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Replace 'notebook1.ipynb' and 'notebook2.ipynb' with the names of your Jupyter notebooks
+NOTEBOOK1="/Users/valenetjong/Bayesian-Optimization-Ferroelectrics/src/ferro-gp_mle_bo_training.ipynb"
+NOTEBOOK2="/Users/valenetjong/Bayesian-Optimization-Ferroelectrics/src/ferro_gp_voltage_calibrations.ipynb"
+
+# Execute the first notebook
+echo "Training..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK1"
+
+# Execute the second notebook
+echo "Calibrating voltages..."
+jupyter nbconvert --to notebook --execute --inplace "$NOTEBOOK2"
+
+echo "Execution complete!"
